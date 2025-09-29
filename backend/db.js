6d810a505419e6ca0a83d2ea75fc1ade908eb2db
Alejandra-Cabrera21 <cabreraalejandra462@gmail.com>
2025-09-28 21:06:11 -0600

    actualizar db

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <- necesario en Render
+    },
+  },
 });
 
 module.exports = sequelize;
