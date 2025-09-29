bcfb6cdfdfa56d0053bfef22a6e38902e2440586
Alejandra-Cabrera21 <cabreraalejandra462@gmail.com>
2025-09-28 20:39:56 -0600

    Cambio a PostgreSQL con pg en Render

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,10 @@
+// backend/db.js
 const { Sequelize } = require("sequelize");
 
-// Render te da la URL en las variables de entorno
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true, // Render usa SSL
-      rejectUnauthorized: false
-    }
-  }
 });
 
 module.exports = sequelize;
