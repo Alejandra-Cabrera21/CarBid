9925f1625d7d3e0ad39a5b223a2b0e8d6399d032
Alejandra-Cabrera21 <cabreraalejandra462@gmail.com>
2025-10-04 19:33:55 -0600

    actualizar cambios

--- backend/models/Bid.js
+++ backend/models/Bid.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
 const User = require("./User");
+const Buyer = require("./Buyer");
 const Auction = require("./Auction");
 
 const Bid = sequelize.define("Bid", {
@@ -8,11 +9,17 @@ const Bid = sequelize.define("Bid", {
   ganada: { type: DataTypes.BOOLEAN, defaultValue: false }
 });
 
-// Relaciones
-Bid.belongsTo(User, { foreignKey: "userId" });
-User.hasMany(Bid, { foreignKey: "userId" });
+// === Relaciones ===
 
+// Relación con Buyer (para mostrar nombre/correo del comprador)
+Bid.belongsTo(Buyer, { foreignKey: "userId" });
+Buyer.hasMany(Bid, { foreignKey: "userId" });
+
+// Relación con Auction (subasta a la que pertenece la puja)
 Bid.belongsTo(Auction, { foreignKey: "auctionId" });
 Auction.hasMany(Bid, { foreignKey: "auctionId" });
+
+// Si todavía usas "User" para otro propósito (por ejemplo login general)
+Bid.belongsTo(User, { foreignKey: "userId", constraints: false });
 
 module.exports = Bid;
---
+++ backend/models/Buyer.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const Buyer = sequelize.define("Buyer", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  correo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: "Buyers",
+  timestamps: true
+});
+
+module.exports = Buyer;
