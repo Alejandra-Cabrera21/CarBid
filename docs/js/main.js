46535d664645c42f75d7ec1ea93bbb284f054ca9
angie-schumann <apschumann8@gmail.com>
2025-10-14 18:41:11 -0600

    index responsivo

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btnRegistro = document.getElementById("btnRegistro");
+  const btnRegistroMobile = document.getElementById("btnRegistroMobile");
 
   if (btnRegistro) {
     btnRegistro.addEventListener("click", () => {
+      window.location.href = "register.html";
+    });
+  }
+  if (btnRegistroMobile) {
+    btnRegistroMobile.addEventListener("click", () => {
       window.location.href = "register.html";
     });
   }
